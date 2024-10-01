606fea96296483fe3d4113d7d88aa13f035fed87
Mohamed Gbr <mohamedgbr20233@gmail.com>
2024-10-01 10:23:47 +0300

    changeing stuff

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="min-h-screen bg-black flex justify-center items-center">
-   <h1 class="text-white animate-spin  transition-all duration-700 text-2xl">
-    Hello from Rizme Back end 
-   </h1>
+    <h1 class="text-green-400">hello from the front end</h1>
   </div>
 </template>
-
