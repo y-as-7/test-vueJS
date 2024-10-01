c826c7c5fa8b0876829ac6bc203c22f808244489
youssef askar <youssefaskar111@outlook.com>
2024-10-01 10:12:39 +0300

    initiate project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-black flex justify-center items-center">
+   <h1 class="text-white animate-spin  transition-all duration-700 text-2xl">
+    Hello from Rizme Back end 
+   </h1>
+  </div>
+</template>
+
